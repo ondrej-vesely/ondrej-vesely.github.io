5173d94c45b1668ff43cc52d1b81675a67ee72eb
ondrej-vesely <ondrej-vesely@seznam.cz>
2020-06-11 00:02:34 +0200

    fix resize

--- globe/globe.js
+++ globe/globe.js
@@ -334,9 +334,10 @@ DAT.Globe = function(container, opts) {
   }
 
   function onWindowResize( event ) {
-    camera.aspect = container.offsetWidth / container.offsetHeight;
+    console.log('resize');
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize( container.offsetWidth, container.offsetHeight );
+    renderer.setSize( window.innerWidth, window.innerHeight );
   }
 
   function zoom(delta) {
